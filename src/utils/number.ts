c5ffdd32194205922eea81888efb1027e485898d
newproplus <92970070+newproplus@users.noreply.github.com>
2024-06-13 07:59:35 +0800

    modify doc

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,7 +1,7 @@
 /*
  * Exited uses a dual license.
- * You may conditionally use exsed under the MIT License,
- * and if you do not meet the conditions, authorization is required
+ * You may conditionally use exsed under the MIT License, and
+ * if you do not meet the conditions, authorization is required.
  *
  * Existing license:
  *     https://github.com/exsied/exsied/blob/main/LICENSE
